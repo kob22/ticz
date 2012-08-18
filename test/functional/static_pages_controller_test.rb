f38fed87de82dc2348e4634233c398dec7525bd4
kob22 <kob2222@gmail.com>
2012-08-18 22:52:23 +0200

    testy poprawione

--- test/functional/static_pages_controller_test.rb
+++ test/functional/static_pages_controller_test.rb
@@ -1,6 +1,7 @@
 require 'test_helper'
 
 class StaticPagesControllerTest < ActionController::TestCase
+include Devise::TestHelpers
   test "should get index" do
     get :index
     assert_response :success
