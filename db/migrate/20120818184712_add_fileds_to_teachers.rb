a62c05c132afa4a87a88fa4b09e55cb379be0599
kob22 <kob2222@gmail.com>
2012-08-18 22:33:46 +0200

    baza

--- db/migrate/20120818184712_add_fileds_to_teachers.rb
+++
@@ -1,7 +0,0 @@
-class AddFiledsToTeachers < ActiveRecord::Migration
-  def change
-    add_column :teachers, :degree, :string
-    add_column :teachers, :name, :string
-    add_column :teachers, :surname, :string
-  end
-end
