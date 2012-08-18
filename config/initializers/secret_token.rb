a5647cbdab526feaee4a40a2cfb955303966c2a5
kob22 <kob2222@gmail.com>
2012-08-18 20:23:11 +0200

    base app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Ticz::Application.config.secret_token = '2ed4e03eba179dc900d1f76bd724cfba6f892262c0f62fb5684ca3791e0b9683418ab89dfa0bd567b9636406dbbd88f3ebb3c27eb278e804d039d4aca646c97e'
